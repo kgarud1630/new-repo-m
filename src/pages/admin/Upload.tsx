5063662f9fed19bcdf24e2c06c56d8154f02fc3b
kgarud1630 <kgarud1630@gmail.com>
2025-05-28 12:35:17 +0530

    Add admin upload page

---
+++ src/pages/admin/Upload.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminUpload = () => {
+  return (
+    <div>
+      <h1>Admin Upload Page</h1>
+      <p>This is where admin can upload content.</p>
+    </div>
+  );
+};
+
+export default AdminUpload;
